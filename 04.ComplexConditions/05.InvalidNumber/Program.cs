80234fadf117b50ce4059a3b7f24f5bfd8483ff7
Someone Else <medical_boy_sf@yahoo.com>
2018-04-18 23:05:30 +0300

    03-05 Exercises

---
+++ 04.ComplexConditions/05.InvalidNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.InvalidNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                if (number > 200 || number < 100)
+                {
+                    Console.WriteLine("invalid");
+                }
+            }
+        }
+    }
+}
