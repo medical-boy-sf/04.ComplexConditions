f181b2a73f58ae9c225b7677466c9adb8772f6be
Someone Else <medical_boy_sf@yahoo.com>
2018-04-18 22:43:33 +0300

    01-02 Exercise

---
+++ 04.ComplexConditions/01.PersonalTitles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01.PersonalTitles
+{
+    class Program
+    {
+        static void Main()
+        {
+            double age = double.Parse(Console.ReadLine());
+            char gender = char.Parse(Console.ReadLine());
+
+            if (gender == 'm')
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+            }
+
+            else
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+        }
+    }
+}
---
+++ 04.ComplexConditions/02.SmallShop/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _02.SmallShop
+{
+    class Program
+    {
+        static void Main()
+        {
+            string product = Console.ReadLine();
+            string city = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (city == "Sofia")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.5;
+                        break;
+                    case "water":
+                        price = 0.8;
+                        break;
+                    case "beer":
+                        price = 1.2;
+                        break;
+                    case "sweets":
+                        price = 1.45;
+                        break;
+                    case "peanuts":
+                        price = 1.60;
+                        break;
+                }
+            }
+
+            else if (city == "Plovdiv")
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.4;
+                        break;
+                    case "water":
+                        price = 0.7;
+                        break;
+                    case "beer":
+                        price = 1.15;
+                        break;
+                    case "sweets":
+                        price = 1.30;
+                        break;
+                    case "peanuts":
+                        price = 1.50;
+                        break;
+                }
+            }
+
+            else
+            {
+                switch (product)
+                {
+                    case "coffee":
+                        price = 0.45;
+                        break;
+                    case "water":
+                        price = 0.7;
+                        break;
+                    case "beer":
+                        price = 1.1;
+                        break;
+                    case "sweets":
+                        price = 1.35;
+                        break;
+                    case "peanuts":
+                        price = 1.55;
+                        break;
+                }
+            }
+
+            double total = quantity * price;
+            Console.WriteLine(total);
+        }
+    }
+}
