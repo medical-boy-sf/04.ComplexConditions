0cd5ec3c4bf31465dc547fe042f7ff9b4bf31c4a
Someone Else <medical_boy_sf@yahoo.com>
2018-04-18 23:44:45 +0300

    06.PointOnRectangleBorder

---
+++ 04.ComplexConditions/07.FruitShop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.FruitShop
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double price = 0;
+
+        }
+    }
+}
