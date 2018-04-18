ae8f52b7ac3f554d679fdc39a379db8cce8b3039
Someone Else <medical_boy_sf@yahoo.com>
2018-04-19 00:16:48 +0300

    07-08 Exercises

--- 04.ComplexConditions/07.FruitShop/Program.cs
+++ 04.ComplexConditions/07.FruitShop/Program.cs
@@ -11,6 +11,157 @@ namespace _07.FruitShop
             double quantity = double.Parse(Console.ReadLine());
             double price = 0;
 
+            switch (fruit)
+            {
+                case "banana":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 2.5;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 2.7;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                case "apple":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 1.2;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 1.25;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                case "orange":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 0.85;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 0.9;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                case "grapefruit":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 1.45;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 1.60;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                case "kiwi":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 2.7;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 3;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                case "pineapple":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 5.5;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 5.6;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                case "grapes":
+                    switch (dayOfWeek)
+                    {
+                        case "Monday":
+                        case "Tuesday":
+                        case "Wednesday":
+                        case "Thursday":
+                        case "Friday":
+                            price = 3.85;
+                            break;
+                        case "Saturday":
+                        case "Sunday":
+                            price = 4.2;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+
+            double total = price * quantity;
+
+            Console.WriteLine($"{total:F2}");
+
         }
     }
 }
---
+++ 04.ComplexConditions/08.TradeComission/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace _08.TradeComission
+{
+    class Program
+    {
+        static void Main()
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commission = 0;
+
+            if (city == "Sofia")
+            {
+                if (sales > 10000)
+                {
+                    commission = 0.12;
+                }
+
+                else if (sales > 1000)
+                {
+                    commission = 0.08;
+                }
+
+                else if (sales > 500)
+                {
+                    commission = 0.07;
+                }
+
+                else if (sales > 0)
+                {
+                    commission = 0.05;
+                }
+
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+
+            else if (city == "Varna")
+            {
+                if (sales > 10000)
+                {
+                    commission = 0.13;
+                }
+
+                else if (sales > 1000)
+                {
+                    commission = 0.10;
+                }
+
+                else if (sales > 500)
+                {
+                    commission = 0.075;
+                }
+
+                else if (sales > 0)
+                {
+                    commission = 0.045;
+                }
+
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+
+            else if (city == "Plovdiv")
+            {
+                if (sales > 10000)
+                {
+                    commission = 0.145;
+                }
+
+                else if (sales > 1000)
+                {
+                    commission = 0.12;
+                }
+
+                else if (sales > 500)
+                {
+                    commission = 0.08;
+                }
+
+                else if (sales > 0)
+                {
+                    commission = 0.055;
+                }
+
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            double total = sales * commission;
+            Console.WriteLine($"{total:F2}");
+        }
+    }
+}
