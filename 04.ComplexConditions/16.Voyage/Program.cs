f331eeaa46723d096c605ada6a9607b827e1f0bb
Someone Else <medical_boy_sf@yahoo.com>
2018-04-22 13:24:22 +0300

    16.Voyage

---
+++ 04.ComplexConditions/16.Voyage/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace _16.Voyage
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            if (budget < 10 || budget > 5000)
+            {
+                return;
+            }
+            string season = Console.ReadLine();
+            double sum = 0;
+
+
+            if (budget > 1000)
+            {
+                sum = budget * 0.9;
+                Console.WriteLine("Somewhere in Europe");
+                Console.WriteLine($"Hotel - {sum:F2}");
+            }
+
+            else if (budget > 100)
+            {
+                if (season == "summer")
+                {
+                    sum = budget * 0.4;
+                    Console.WriteLine("Somewhere in Balkans");
+                    Console.WriteLine($"Camp - {sum:F2}");
+                }
+
+                else
+                {
+                    sum = budget * 0.8;
+                    Console.WriteLine("Somewhere in Balkans");
+                    Console.WriteLine($"Hotel - {sum:F2}");
+                }
+            }
+
+            else
+            {
+                if (season == "summer")
+                {
+                    sum = budget * 0.3;
+                    Console.WriteLine("Somewhere in Bulgaria");
+                    Console.WriteLine($"Camp - {sum:F2}");
+                }
+
+                else
+                {
+                    sum = budget * 0.7;
+                    Console.WriteLine("Somewhere in Bulgaria");
+                    Console.WriteLine($"Hotel - {sum:F2}");
+                }
+            }
+        }
+    }
+}
